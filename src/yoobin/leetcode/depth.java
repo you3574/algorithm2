f86cda6c97244b35c11c3196e01e4f7dcd2bbfd1
DESKTOP-CPF5K67\yoobin <you3574@naver.com>
2019-11-20 18:20:39 +0900

    19.11.20 유빈

---
+++ src/yoobin/leetcode/depth.java
@@ -0,0 +1,26 @@
+package yoobin.leetcode;
+import java.util.*;
+public class depth {
+	  public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	 class Solution {
+	    public int maxDepth(TreeNode root) {
+	        if(root==null)
+	            return 0;
+	        else if(root.left==null && root.right==null)
+	            return 1;
+	        else{
+	            return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+	        }
+	    }
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+}
