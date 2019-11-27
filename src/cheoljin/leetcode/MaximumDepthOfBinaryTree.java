23f505665e9dc9a9f5457bcc494d387358f8509b
CheolJin Kang <kcj091857@gmail.com>
2019-11-27 10:49:51 +0900

    resolve algorithm MaximumDepthOfBinaryTree in leetcode

---
+++ src/cheoljin/leetcode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,33 @@
+package cheoljin.leetcode;
+
+public class MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        TreeNode root1 = new TreeNode(0);
+
+        System.out.println(maxDepth(root1));
+    }
+
+    static int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
